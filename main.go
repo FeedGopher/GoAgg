bd16dadc48dd2e3ccffec89544feba35241a6dfb
Jonathan Krone <jkrone@vt.edu>
2016-05-21 00:10:07 -0500

    (init) completion of first golang module

--- main.go
+++ main.go
@@ -5,7 +5,27 @@ import (
 	"http"
 )
 
+// package level variables
 // var dbConnection psqlConn
+
+// defaults
+/*
+	int: 0
+	uint: 0
+	string: ""
+	bool: false
+	byte:
+	rune: 
+*/
+
+var (
+	neature int = math.E
+	MaxInt uint64 = 1<<64 - 1
+	sign byte
+	glyph rune
+)
+
+// Outside a function, every statement must begin with a keyword
 
 func add(x, y int) int {
 	return x + y
@@ -29,8 +49,18 @@ func subtract(x int, y int) (int, int) {
 }
 
 func main() {
+	// if an initializer (value) is provided, types may be omitted.
+	var jelly, pb, raisins = 2, true, false
+
 	fmt.Println(add(42, 13))
 
+	// := indicates implicit typing
 	pos, neg := subtract(42, 13)
 	fmt.Println(pos, neg)
+
+	const f = "%T(%v)\n"
+	fmt.Printf(f, neature, neature)
+	fmt.Printf(f, MaxInt, MaxInt)
+	fmt.Printf(f, sign, sign)
+	fmt.Printf(f, glyph, glyph)
 }
