6f7c60e475ed7708f5a6a7f54e15be54779a7852
Jonathan Krone <jkrone@vt.edu>
2016-05-20 23:57:00 -0500

    (init) Additional notes and exercise with go.

--- main.go
+++ main.go
@@ -5,7 +5,32 @@ import (
 	"http"
 )
 
-func main() {
-	
+// var dbConnection psqlConn
 
+func add(x, y int) int {
+	return x + y
+}
+
+struct feed (
+
+)
+
+func fetchFeed(uri string) (*feed) {
+
+}
+
+
+func subtract(x int, y int) (int, int) {
+	primary = x - y
+	secondary = y - x
+
+	// empty, or naked, return returns named variables
+	return
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+
+	pos, neg := subtract(42, 13)
+	fmt.Println(pos, neg)
 }
