cc2ea67fda331094a133652f4df27bf63bff52bb
Jonathan Krone <jkrone@vt.edu>
2016-05-23 22:26:24 -0500

    (feat) Maybe progress on XML

--- main.go
+++ main.go
@@ -10,85 +10,78 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	// "io/ioutil"
+	"io/ioutil"
 	// "encoding/json"
 
-	"github.com/clbanning/mxj"
+	// "github.com/clbanning/mxj"
 	// "database/sql"
 	// "github.com/lib/pq"
 )
 
 func main() {
 	response, err := http.Get("https://www.blogger.com/feeds/8100407163665430627/posts/default")
-
-	// response, err := http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Println("ATOM GET error:", err)
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
-	fmt.Println("response body:", response.Body)
+	fmt.Printf("response body: %s", response.Body)
 
 	// ByteBody, err := ioutil.ReadAll(response.Body)
-	fmt.Printf("MXJ: %#v", mxj)
 
-	// Unmarshall XML-encoded response body to a json-like map.
-	// mBody, err := mxj.NewMapReader(response.Body)
+	// // Unmarshall XML-encoded response body to a json-like map.
+	// // mBody, err := mxj.NewMapReader(response.Body)
+	// if err != nil {
+	// 	fmt.Println("Feed response-to-XML error:", err)
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("mxj'd response body: %s", ByteBody)
+
+	xmlMap, err := mxj.NewMapXmlReader(response.Body)
 	if err != nil {
-		fmt.Println("Feed response-to-XML error:", err)
-		log.Fatal(err)
+		fmt.Println("Error creating map from XML reader", err)
 	}
-	// fmt.Printf("mxj'd response body: %s", mBody)
+	fmt.Printf("mxj XML map: %S", xmlMap)
+
+	
+
+	JSON writer
+
+// 	// func to handle Map value from XML Reader
+// 	func maphandler(m mxj.Map) bool {
+
+// 		// marshal Map as JSON
+// 		jsonVal, err := m.Json()
+// 		if err != nil {
+// 			fmt.Println("JSON marshalling failed.", err)
+// 			log.Fatal(err)
+// 			return false // stop further processing of XML Reader
+// 		}
+
+// 		// write JSON somewhere
+// 		json, err = jsonWriter.Write(jsonVal)
+// 		if err != nil {
+// 			fmt.Println("Writing marshalled JSON failed.", err)
+// 			log.Fatal(err)
+// 			return false // stop further processing of XML Reader
+// 		}
+
+// 		return true // continue - get next XML from Reader
+// 	}
+
+// 	// func to handle error from unmarshaling XML Reader
+// 	func errhandler(errVal error) bool {
+// 		fmt.Println("Error caught by HandleXmlReader", errVal.Error());
+// 		return true // continue
+// 	}
+
+// 	err := mxj.HandleXmlReader(xmlReader, maphandler, errhandler)
+// 	if err != nil {
+// 		fmt.Println("Hamdle XML reader failed", err)
+// 	}
+
 }
 
-// need somewhere to write the JSON.
-		var jsonWriter io.Writer
-
-		// probably want to log any errors in reading the XML stream
-		var xmlErrLogger io.Writer
-
-		// func to handle Map value from XML Reader
-		func maphandler(m mxj.Map) bool {
-			// marshal Map as JSON
-			jsonVal, err := m.Json()
-			if err != nil {
-				// log error
-				return false // stops further processing of XML Reader
-			}
-
-			// write JSON somewhere
-			_, err = jsonWriter.Write(jsonVal)
-			if err != nil {
-				// log error
-				return false // stops further processing of XML Reader
-			}
-
-			// continue - get next XML from Reader
-			return true
-		}
-
-		// func to handle error from unmarshaling XML Reader
-		func errhandler(errVal error) bool {
-			// log error somewhere
-			_, err := xmlErrLogger.Write([]byte(errVal.Error()))
-			if err != nil {
-				// log error
-				return false // stops further processing of XML Reader
-			}
-
-			// continue processing
-			return true
-		}
-
-		// func that starts bulk processing of the XML
-			...
-			// set up io.Reader for XML data - perhaps an os.File
-			...
-			err := mxj.HandleXmlReader(xmlReader, maphandler, errhandler)
-			if err != nil {
-				// handle error
-			}
-			...
 
 /*
 
