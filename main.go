f7de85ecbe1d42b0dc039e5ba16bf6ab3a21e7f0
Jonathan Krone <jkrone@vt.edu>
2016-06-28 18:29:09 -0500

    (doc) Cleanup and documentation. I think the next step should be to get a server running, open an endpoint on this function's results, and display on a client.

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	// "bufio"
 	"os"
 
 	"github.com/clbanning/mxj"
@@ -27,58 +26,16 @@ func main() {
 	defer response.Body.Close()
 	fmt.Printf("response body: %s", response.Body)
 
-	// ByteBody, err := ioutil.ReadAll(response.Body)
-
-	// // Unmarshall XML-encoded response body to a json-like map.
-	// // mBody, err := mxj.NewMapReader(response.Body)
-	// if err != nil {
-	// 	fmt.Println("Feed response-to-XML error:", err)
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("mxj'd response body: %s", ByteBody)
-
+	// Use mxj package to translate XML structured bytes to a map[string]interface{}
 	xmlMap, err := mxj.NewMapXmlReader(response.Body)
 	if err != nil {
 		fmt.Println("Error creating map from XML reader", err)
 	}
-	fmt.Printf("mxj XML map: %S", xmlMap)
 
+	// Output map[string]interface{...} to file as JSON.
 	f, err := os.Create("output.json")	
+	if err != nil {
+		fmt.Println("Error creating test output file.", err)
+	}
 	xmlMap.JsonIndentWriter(f, "", "\t", true)
-
-	// JSON writer
-
-	// 	// func to handle Map value from XML Reader
-	// 	func maphandler(m mxj.Map) bool {
-
-	// 		// marshal Map as JSON
-	// 		jsonVal, err := m.Json()
-	// 		if err != nil {
-	// 			fmt.Println("JSON marshalling failed.", err)
-	// 			log.Fatal(err)
-	// 			return false // stop further processing of XML Reader
-	// 		}
-
-	// 		// write JSON somewhere
-	// 		json, err = jsonWriter.Write(jsonVal)
-	// 		if err != nil {
-	// 			fmt.Println("Writing marshalled JSON failed.", err)
-	// 			log.Fatal(err)
-	// 			return false // stop further processing of XML Reader
-	// 		}
-
-	// 		return true // continue - get next XML from Reader
-	// 	}
-
-	// 	// func to handle error from unmarshaling XML Reader
-	// 	func errhandler(errVal error) bool {
-	// 		fmt.Println("Error caught by HandleXmlReader", errVal.Error());
-	// 		return true // continue
-	// 	}
-
-	// 	err := mxj.HandleXmlReader(xmlReader, maphandler, errhandler)
-	// 	if err != nil {
-	// 		fmt.Println("Hamdle XML reader failed", err)
-	// 	}
-
 }
